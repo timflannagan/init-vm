deeef80ebfde1dc2dac6ec7235b68af14db507dd
timflannagan1 <timflannagan@gmail.com>
2018-08-06 15:13:04 -0400

    Update script

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 chmod +x install.sh
 sudo yum install ansible -y
-git clone https://github.com/timflannagan1/linux-storage-role ~
+git clone https://github.com/timflannagan1/linux-storage-role ~/Documents/linux-storage-role/
 git remote add upstream https://github.com/dwlehman/linux-storage-role
