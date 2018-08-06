8f27dc95534f06207a2a35dc9a4979cd0bb5ce05
timflannagan1 <timflannagan@gmail.com>
2018-08-06 15:09:56 -0400

    Add permissions

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 
+chmod +x install.sh
 sudo yum install ansible -y
-git clone https://github.com/timflannagan1/linux-storage-role ~/
+git clone https://github.com/timflannagan1/linux-storage-role ~
 git remote add upstream https://github.com/dwlehman/linux-storage-role
