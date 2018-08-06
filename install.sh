2ca3ba12fb59da4359ee40bb3af548553c5c8562
timflannagan1 <timflannagan@gmail.com>
2018-08-06 15:06:33 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+sudo yum install ansible -y
+git clone https://github.com/timflannagan1/linux-storage-role ~/
+git remote add upstream https://github.com/dwlehman/linux-storage-role
