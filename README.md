c3d1b758523592eab16e05cabb640bd43977534e
timflannagan1 <timflannagan@gmail.com>
2018-08-07 12:45:27 -0400

    Update README and git-install

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Test
+Notes:
+===============
+1. Installs and configures emacs to be the default editor
+2. Installs ansible and clones the necessary role repo
+3. Install editor of choice
--- git-install.sh
+++ git-install.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/bash
 
 sudo yum install ansible -y
+sudo yum install emacs -y
 git clone https://github.com/timflannagan1/linux-storage-role ~/Documents/linux-storage-role/
+git config --global user.editor "emacs"
