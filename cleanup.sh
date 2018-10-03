a0b15e27667b928d7e5c06e57ccd9da5175f70fd
timflannagan1 <timflannagan@gmail.com>
2018-10-03 12:15:55 -0400

    Force cleanup.sh to override fstab file

--- cleanup.sh
+++ cleanup.sh
@@ -6,6 +6,9 @@ sudo umount /opt/test*
 # get rid of any lvs
 sudo lvremove /dev/mapper/*test* -y
 
+# this is an awful idea
+cp -f /etc/fstab fstab.txt
+
 # TO-DO:
 # 1. modify /etc/fstab file
 # 2. add umount for /dev/ disk device
