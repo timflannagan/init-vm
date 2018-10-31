94947ab454056834ca1eaa368227aec59787b924
timflannagan1 <timflannagan@gmail.com>
2018-10-31 16:20:03 -0400

    Add -y option to yum and hardcode the directory layout of the storage playbook path

--- quick_test.sh
+++ quick_test.sh
@@ -3,4 +3,4 @@
 USAGE="Enter the package name after the shell executable."
 echo $USAGE
 
-sudo yum remove $1 ; clear ; sudo ansible-playbook tests/test.yml ; lsblk -f
+sudo yum remove $1 -y ; clear ; sudo ansible-playbook ~/Documents/storage/tests/test.yml ; lsblk -f
