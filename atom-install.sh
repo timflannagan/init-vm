df57f40c75278dafb00737beef4f28662abd0ef7
timflannagan1 <timflannagan@gmail.com>
2018-11-01 13:57:05 -0400

    Update shebang to work with mac

--- atom-install.sh
+++ atom-install.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
 sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey 
 sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
